64282a285ff53863c2a90d85515c18bf14950e0c
Leland Capps <lcapps5600@gmail.com>
2023-10-01 13:41:25 -0500

    completed an assigmenet

--- express/app.js
+++ express/app.js
@@ -1,13 +1,42 @@
 
+// const express = require('express')
+// const app = express()
+
+// app.use("/add-product", (req, res, next) => {
+//     res.send("<h1>This is the add a product page.")
+// })
+// app.use((req, res, next) => {
+//     console.log("in middleware 2")
+//     res.send("<h1>Hello this is a new page</h1>")
+// })
+
+
+// app.listen(3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
 const express = require('express')
 const app = express()
 
-app.use("/add-product", (req, res, next) => {
-    res.send("<h1>This is the add a product page.")
+
+app.use("/users", (req, res, next) => {
+    console.log('middleware 1')
+    res.send("<h1>This is the users tab")
 })
-app.use((req, res, next) => {
-    console.log("in middleware 2")
-    res.send("<h1>Hello this is a new page</h1>")
+
+app.use("/", (req, res, next) => {
+    console.log('middleware 2')
+    res.send("<h1>this response is coming from middleWare 2")
 })
 
 
