af534bfd23ddc58716ade3d16c25d6817800c766
Leland Capps <lcapps5600@gmail.com>
2023-10-04 15:22:20 -0500

    serving HTML w CSS

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,10 +1,14 @@
 const path = require('path')
 const express = require('express')
+
+const rootDir = require('../util/path')
+
 const router = express.Router()
 
 router.get("/add-product", (req, res, next) => {
     console.log("in another middleware")
-    res.sendFile(__dirname, '../', 'views', 'add-product.html')
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    // res.sendFile(rootDir, 'views', 'add-product.html')
 })
 router.post('/add-product', (req, res, next)=>{
     console.log(req.body)
