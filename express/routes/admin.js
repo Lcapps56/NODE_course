9bea665ca7b39f73b748e77915ce26059e0394f2
Leland Capps <lcapps5600@gmail.com>
2023-10-12 13:58:24 -0500

    working with data within pug

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -17,6 +17,7 @@ router.get('/add-product', (req, res, next) => {
 router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect('/');
+
 });
 
 exports.routes = router;
