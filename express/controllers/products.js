ef5e1904454f3fe42f03b2d2fd3c9e4bcb6db4d2
Leland Capps <lcapps5600@gmail.com>
2023-10-16 14:55:01 -0500

    models and controllers added

--- express/controllers/products.js
+++ express/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product')
 
 exports.getAddProductsPage = (req, res, next) => {
     res.render('add-product', {
@@ -11,11 +11,14 @@ exports.getAddProductsPage = (req, res, next) => {
   }
 
 exports.addProduct = (req, res, next) => {
-products.push({ title: req.body.title });
-res.redirect('/');
+    const product = new Product(req.body.title)
+    product.save()
+    res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll()
+
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
